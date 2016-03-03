a68fa5febdcb253d4def4af6ff2abdf92111b8bf
Tarliton Godoy <tarlitongodoy@gmail.com>
2016-03-03 20:05:42 -0300

    refactor vector

--- vector.go
+++ vector.go
@@ -19,120 +19,84 @@ func NewVector(x, y float64) Vector {
 	return Vector{X: x, Y: y}
 }
 
-//Copy the value of other vector to a new one.
-func (vector Vector) Copy(other Vector) Vector {
-	vector.X = other.X
-	vector.Y = other.Y
-	return vector
+//Copy the value of another vector to a new one.
+func (vector Vector) Copy(another Vector) Vector {
+	return Vector{another.X, another.Y}
 }
 
 //Clone this vector coordinates to a new vector with the same coordinates as this one.
 func (vector Vector) Clone() Vector {
-	another := Vector{}.Copy(vector)
-	return another
+	return Vector{}.Copy(vector)
 }
 
 //Perp returns a new vector perpendicular from this one.
 func (vector Vector) Perp() Vector {
-	x := vector.X
-	vector.X = vector.Y
-	vector.Y = -x
-	return vector
+	return Vector{vector.Y, -vector.X}
 }
 
 //Rotate returns a new vector rotated counter-clockwise by the specified number of radians.
 func (vector Vector) Rotate(angle float64) Vector {
-	x := vector.X
-	y := vector.Y
-	vector.X = x*math.Cos(angle) - y*math.Sin(angle)
-	vector.Y = x*math.Sin(angle) + y*math.Cos(angle)
-	return vector
+	return Vector{
+		vector.X*math.Cos(angle) - vector.Y*math.Sin(angle),
+		vector.X*math.Sin(angle) + vector.Y*math.Cos(angle)}
 }
 
 //Reverse returns a new vector that is reversed from this one.
 func (vector Vector) Reverse() Vector {
-	vector.X = -vector.X
-	vector.Y = -vector.Y
-	return vector
+	return Vector{-vector.X, -vector.Y}
 }
 
 //Normalize returns a new unit-length vector.
 func (vector Vector) Normalize() Vector {
 	d := vector.Len()
-	if d > 0 {
-		vector.X /= d
-		vector.Y /= d
-	}
-	return vector
+	return Vector{vector.X / d, vector.Y / d}
 }
 
 //Add returns a new vector with the result of adding another vector to this one.
-func (vector Vector) Add(other Vector) Vector {
-	vector.X += other.X
-	vector.Y += other.Y
-	return vector
+func (vector Vector) Add(another Vector) Vector {
+	return Vector{vector.X + another.X, vector.Y + another.Y}
 }
 
 //Sub returns a new vector that is the result of subtracting another vector from this one.
-func (vector Vector) Sub(other Vector) Vector {
-	vector.X -= other.X
-	vector.Y -= other.Y
-	return vector
+func (vector Vector) Sub(another Vector) Vector {
+	return Vector{vector.X - another.X, vector.Y - another.Y}
 }
 
 //Scale returns a new vector scaled in the direction of X and Y by value x
 func (vector Vector) Scale(x float64) Vector {
-	vector.X *= x
-	vector.Y *= x
-	return vector
+	return Vector{vector.X * x, vector.Y * x}
 }
 
 //ScaleDifferent returns a new vector scaled in the direction of X and Y by value x and y respectively
 func (vector Vector) ScaleDifferent(x, y float64) Vector {
-	vector.X *= x
-	vector.Y *= y
-	return vector
+	return Vector{vector.X * x, vector.Y * y}
 }
 
 //Project this vector onto another one
-func (vector Vector) Project(other Vector) Vector {
-	amt := vector.Dot(other) / other.Len2()
-	vector.X = amt * other.X
-	vector.Y = amt * other.Y
-	return vector
+func (vector Vector) Project(another Vector) Vector {
+	amt := vector.Dot(another) / another.Len2()
+	return Vector{amt * another.X, amt * another.Y}
 }
 
 //ProjectN this vector onto a unit vector
-func (vector Vector) ProjectN(other Vector) Vector {
-	amt := vector.Dot(other)
-	vector.X = amt * other.X
-	vector.Y = amt * other.Y
-	return vector
+func (vector Vector) ProjectN(another Vector) Vector {
+	amt := vector.Dot(another)
+	return Vector{amt * another.X, amt * another.Y}
 }
 
 //Reflect this vector on an arbitrary axis vector
 func (vector Vector) Reflect(axis Vector) Vector {
-	x := vector.X
-	y := vector.Y
-	resultVector := vector.Project(axis).Scale(2)
-	resultVector.X -= x
-	resultVector.Y -= y
-	return resultVector
+	return vector.Project(axis).Scale(2).Sub(vector)
 }
 
 //ReflectN this vector on an arbitrary axis unit vector
 func (vector Vector) ReflectN(axis Vector) Vector {
-	x := vector.X
-	y := vector.Y
-	resultVector := vector.ProjectN(axis).Scale(2)
-	resultVector.X -= x
-	resultVector.Y -= y
-	return resultVector
+	return vector.ProjectN(axis).Scale(2).Sub(vector)
 }
 
 //Dot returns the dot product of this vector and another
-func (vector Vector) Dot(other Vector) float64 {
-	return vector.X*other.X + vector.Y*other.Y
+func (vector Vector) Dot(another Vector) float64 {
+	return vector.X*another.X + vector.Y*another.Y
 }
 
 //Len2 returns the squared length of this vector
--- vector_test.go
+++ vector_test.go
@@ -21,39 +21,45 @@ func TestCreateVectorXZeros(t *testing.T) {
 }
 
 func TestCreateVectorXNumber(t *testing.T) {
-	vector := collision2d.NewVector(15.12312, 12312.1)
-	assert.Equal(t, float64(15.12312), vector.X, "they should be equal")
-	assert.Equal(t, float64(12312.1), vector.Y, "they should be equal")
+	vector := collision2d.NewVector(3.46, 2.18)
+	assert.Equal(t, float64(3.46), vector.X, "they should be equal")
+	assert.Equal(t, float64(2.18), vector.Y, "they should be equal")
 }
 
 func TestVectorCopy(t *testing.T) {
-	vector1 := collision2d.Vector{15.12312, 12312.1}
-	vector2 := collision2d.Vector{}
+	vector1 := collision2d.NewVector(3.46, 2.18)
+	vector2 := collision2d.NewVector(-5.1, 4.8)
 
 	vector3 := vector1.Copy(vector2)
 
-	assert.Equal(t, float64(0), vector3.X, "they should be equal")
-	assert.Equal(t, float64(0), vector3.Y, "they should be equal")
+	assert.Equal(t, float64(3.46), vector1.X, "they should be equal")
+	assert.Equal(t, float64(2.18), vector1.Y, "they should be equal")
+
+	assert.Equal(t, float64(-5.1), vector2.X, "they should be equal")
+	assert.Equal(t, float64(4.8), vector2.Y, "they should be equal")
+
+	assert.Equal(t, float64(-5.1), vector3.X, "they should be equal")
+	assert.Equal(t, float64(4.8), vector3.Y, "they should be equal")
 }
 
 func TestVectorClone(t *testing.T) {
-	vector1 := collision2d.Vector{15.12312, 12312.1}
+	vector1 := collision2d.NewVector(3.46, 2.18)
 
 	vector2 := vector1.Clone()
 
-	assert.Equal(t, float64(15.12312), vector2.X, "they should be equal")
-	assert.Equal(t, float64(12312.1), vector2.Y, "they should be equal")
+	assert.Equal(t, float64(3.46), vector2.X, "they should be equal")
+	assert.Equal(t, float64(2.18), vector2.Y, "they should be equal")
 }
 
 func TestVectorPerp(t *testing.T) {
-	vector := collision2d.Vector{0, 1}
-	vector2 := vector.Perp()
-	assert.Equal(t, float64(1), vector2.X, "X should be 1")
-	assert.Equal(t, float64(0), vector2.Y, "Y should be 0")
+	vector1 := collision2d.NewVector(3.46, 2.18)
+	vector2 := vector1.Perp()
+	assert.Equal(t, float64(2.18), vector2.X, "X should be 1")
+	assert.Equal(t, float64(-3.46), vector2.Y, "Y should be 0")
 }
 
 func TestVectorRotate(t *testing.T) {
-	vector := collision2d.Vector{5.5, 4.7}
+	vector := collision2d.NewVector(5.5, 4.7)
 	vector2 := vector.Rotate(math.Pi)
 	check1 := nearlyEqual(vector2.X, float64(-5.5), float64(0.00001))
 	check2 := nearlyEqual(vector2.Y, float64(-4.7), float64(0.00001))
@@ -62,83 +68,83 @@ func TestVectorRotate(t *testing.T) {
 }
 
 func TestVectorReverse(t *testing.T) {
-	vector := collision2d.Vector{1, -5}
+	vector := collision2d.NewVector(1, -5)
 	vector2 := vector.Reverse()
 	assert.Equal(t, float64(-1), vector2.X, "should be -1")
 	assert.Equal(t, float64(5), vector2.Y, "should be 5")
 }
 func TestVectorNormalize(t *testing.T) {
-	vector := collision2d.Vector{41, 123.123}
+	vector := collision2d.NewVector(41, 123.123)
 	vector2 := vector.Normalize()
 	assert.Equal(t, float64(1), vector2.Len(), "length should be 1")
 }
 func TestVectorAdd(t *testing.T) {
-	vector1 := collision2d.Vector{15, 5}
-	vector2 := collision2d.Vector{25, 45}
+	vector1 := collision2d.NewVector(15, 5)
+	vector2 := collision2d.NewVector(25, 45)
 	vector3 := vector1.Add(vector2)
 	assert.Equal(t, float64(40), vector3.X, "should sum right")
 	assert.Equal(t, float64(50), vector3.Y, "should sum right")
 }
 func TestVectorSub(t *testing.T) {
-	vector1 := collision2d.Vector{15, 5}
-	vector2 := collision2d.Vector{25, 45}
+	vector1 := collision2d.NewVector(15, 5)
+	vector2 := collision2d.NewVector(25, 45)
 	vector3 := vector1.Sub(vector2)
 	assert.Equal(t, float64(-10), vector3.X, "should sum right")
 	assert.Equal(t, float64(-40), vector3.Y, "should sum right")
 }
 func TestVectorScale(t *testing.T) {
-	vector1 := collision2d.Vector{15, 5}
+	vector1 := collision2d.NewVector(15, 5)
 	vector2 := vector1.Scale(2)
 	assert.Equal(t, float64(30), vector2.X, "should sum right")
 	assert.Equal(t, float64(10), vector2.Y, "should sum right")
 }
 func TestVectorScaleDifferent(t *testing.T) {
-	vector1 := collision2d.Vector{15, 5}
+	vector1 := collision2d.NewVector(15, 5)
 	vector2 := vector1.ScaleDifferent(2, 5)
 	assert.Equal(t, float64(30), vector2.X, "should sum right")
 	assert.Equal(t, float64(25), vector2.Y, "should sum right")
 }
 func TestVectorProject(t *testing.T) {
-	vector1 := collision2d.Vector{15, 5}
-	vector2 := collision2d.Vector{0, 20}
+	vector1 := collision2d.NewVector(15, 5)
+	vector2 := collision2d.NewVector(0, 20)
 	vector3 := vector1.Project(vector2)
 	assert.Equal(t, float64(0), vector3.X, "should sum right")
 	assert.Equal(t, float64(5), vector3.Y, "should sum right")
 }
 func TestVectorProjectN(t *testing.T) {
-	vector1 := collision2d.Vector{15, 5}
-	vector2 := collision2d.Vector{0, 20}
+	vector1 := collision2d.NewVector(15, 5)
+	vector2 := collision2d.NewVector(0, 20)
 	vector3 := vector1.ProjectN(vector2)
 	assert.Equal(t, float64(0), vector3.X, "should sum right")
 	assert.Equal(t, float64(2000), vector3.Y, "should sum right")
 }
 func TestVectorReflect(t *testing.T) {
-	vector1 := collision2d.Vector{15, 5}
-	vector2 := collision2d.Vector{0, 20}
+	vector1 := collision2d.NewVector(15, 5)
+	vector2 := collision2d.NewVector(0, 20)
 	vector3 := vector1.Reflect(vector2)
 	assert.Equal(t, float64(-15), vector3.X, "should sum right")
 	assert.Equal(t, float64(5), vector3.Y, "should sum right")
 }
 func TestVectorReflectN(t *testing.T) {
-	vector1 := collision2d.Vector{15, 5}
-	vector2 := collision2d.Vector{0, 20}
+	vector1 := collision2d.NewVector(15, 5)
+	vector2 := collision2d.NewVector(0, 20)
 	vector3 := vector1.ReflectN(vector2)
 	assert.Equal(t, float64(-15), vector3.X, "should sum right")
 	assert.Equal(t, float64(3995), vector3.Y, "should sum right")
 }
 func TestVectorDot(t *testing.T) {
-	vector1 := collision2d.Vector{-4, -9}
-	vector2 := collision2d.Vector{-1, 2}
+	vector1 := collision2d.NewVector(-4, -9)
+	vector2 := collision2d.NewVector(-1, 2)
 	result := vector1.Dot(vector2)
 	assert.Equal(t, float64(-14), result, "should sum right")
 }
 func TestVectorLen(t *testing.T) {
-	vector1 := collision2d.Vector{0, 5}
+	vector1 := collision2d.NewVector(0, 5)
 	result := vector1.Len()
 	assert.Equal(t, float64(5), result, "should sum right")
 }
 func TestVectorLen2(t *testing.T) {
-	vector1 := collision2d.Vector{0, 5}
+	vector1 := collision2d.NewVector(0, 5)
 	result := vector1.Len2()
 	assert.Equal(t, float64(25), result, "should sum right")
 }
